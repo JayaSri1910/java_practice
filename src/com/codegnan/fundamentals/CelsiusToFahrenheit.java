3fb2db645cfb4f02730489c5df4da10eb7a532c9
JayaSri1910 <gandaboinajayasri@gmail.com>
2026-05-28 15:25:27 +0530

    feat:CelsiusToFahrenheit

---
+++ src/com/codegnan/fundamentals/CelsiusToFahrenheit.java
@@ -0,0 +1,14 @@
+package com.codegnan.fundamentals;
+import java.util.Scanner;
+public class CelsiusToFahrenheit {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Temperature in celsius:");
+		int celsius=sc.nextInt();
+		double fahrenheit=celsius*9/5+32;
+		System.out.println("Fahrenheit is:"+fahrenheit);
+
+	}
+
+}
