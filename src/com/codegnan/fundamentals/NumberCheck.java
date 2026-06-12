ce675b68a83797a4dcf04a9782b9aabcade99637
JayaSri1910 <gandaboinajayasri@gmail.com>
2026-06-12 15:23:59 +0530

    feat: add NumberCheck program

---
+++ src/com/codegnan/fundamentals/NumberCheck.java
@@ -0,0 +1,26 @@
+package com.codegnan.fundamentals;
+
+import java.util.Scanner;
+
+public class NumberCheck {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=sc.nextInt();
+		if(num<0) {
+			System.out.println("Negative Number");
+		}
+		else { 
+	        if(num>0) {
+			System.out.println("Positive Number");
+		}
+	        else {
+	        	System.out.println("Zero");
+	        }
+		}
+		sc.close();
+
+	}
+
+}
