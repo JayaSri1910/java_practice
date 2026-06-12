9bbdd5e88231be679e5180c1a9b4dfd3f5f900f4
JayaSri1910 <gandaboinajayasri@gmail.com>
2026-06-12 15:23:58 +0530

    feat: add GeometryApp program

---
+++ src/com/codegnan/fundamentals/GeometryApp.java
@@ -0,0 +1,26 @@
+package com.codegnan.fundamentals;
+
+import java.util.Scanner;
+
+public class GeometryApp {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		if(a==b && b==c && c==d && d==a) {
+			System.out.println("Square");
+		}else {
+			if(a==c && b==d) {
+				System.out.println("Rectangle");
+			}else {
+				System.out.println("other");
+			}
+		}
+		sc.close();
+
+	}
+
+}
