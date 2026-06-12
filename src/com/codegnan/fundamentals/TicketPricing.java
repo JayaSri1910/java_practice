092c9ae38f660dc7dd9b1c77f3dc9352cae56d3c
JayaSri1910 <gandaboinajayasri@gmail.com>
2026-06-12 15:24:01 +0530

    feat: add TicketPricing program

---
+++ src/com/codegnan/fundamentals/TicketPricing.java
@@ -0,0 +1,16 @@
+package com.codegnan.fundamentals;
+
+import java.util.Scanner;
+
+public class TicketPricing {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double basePrice=5000;
+		System.out.println("Enter passengerType(child/adult/senior)");
+		String passengerType=
+		sc.close();
+
+	}
+
+}
