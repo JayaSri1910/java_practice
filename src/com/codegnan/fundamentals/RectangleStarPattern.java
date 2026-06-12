595d5ab6814fcfff5bb4889d82bf741a798b1130
JayaSri1910 <gandaboinajayasri@gmail.com>
2026-06-12 15:24:00 +0530

    feat: add RectangleStarPattern program

---
+++ src/com/codegnan/fundamentals/RectangleStarPattern.java
@@ -0,0 +1,22 @@
+package com.codegnan.fundamentals;
+import java.util.Scanner;
+public class RectangleStarPattern {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of rows:");
+		int rows=sc.nextInt();
+		System.out.println("Enter number of columns:");
+		int cols=sc.nextInt();
+		for(int i=1;i<=rows;i++) {
+			for(int j=1;j<=cols;j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+
+	}
+
+}
