ca0ccc293171bf9b98b91c1ae7f4b19ea284e36c
JayaSri1910 <gandaboinajayasri@gmail.com>
2026-06-19 11:54:25 +0530

    feat(day-7): update TaxBracket program

--- src/com/codegnan/fundamentals/TaxBracket.java
+++ src/com/codegnan/fundamentals/TaxBracket.java
@@ -1,3 +1,5 @@
+
+
 package com.codegnan.fundamentals;
 
 import java.util.Scanner;
@@ -7,8 +9,7 @@ public class TaxBracket {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		double income=sc.nextDouble();
-		String res=(income<3000000)?income*0.05:(income<3000000 && income>1000000)?income*0.10:()
-		sc.close();
+		
 
 	}
 
